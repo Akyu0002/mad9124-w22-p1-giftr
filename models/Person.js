b1ebfd5bc487b0463c11f22745f44a0529000f08
Akyu0002 <akyu0002@algonquinlive.com>
2022-04-05 13:54:16 -0400

    Create Person model and add properties

---
+++ models/Person.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { giftSchema } from "./Gift.js";
+
+const personSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, max: 254 },
+    birthDate: { type: Date, required: true },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      //default: , // Not sure what to set this as for "Current User"
+    },
+    sharedWith: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    gifts: [giftSchema],
+    imageUrl: { type: String, max: 1024 },
+    createdAt: { type: Date, default: Date.now() },
+    updatedAt: { type: Date, default: Date.now() },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Model = mongoose.model("Person", personSchema);
+
+export default Model;
