050fc26e68582fb677ac5f6c23982628ecb7e49a
tran0460 <tran0460@algonquinlive.com>
2022-04-11 19:07:20 -0400

    created authGifts middleware

---
+++ middleware/authGifts.js
@@ -0,0 +1,20 @@
+import User from "../models/User.js";
+import Person from "../models/Person.js";
+
+export default async function (req, res, next) {
+  const person = await Person.findById(req.params.id);
+  if (person.sharedWith.includes(req.user._id) || person.owner === req.user._id) {
+    next();
+  } else {
+    return res.status(403).send({
+      errors: [
+        {
+          status: "403",
+          title: "Access Denied",
+          description:
+            "Access restricted! You do not have permission to do this.",
+        },
+      ],
+    });
+  }
+}
