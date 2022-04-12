74f18a03eb82346d66339281ba8b33ee40958484
Akyu0002 <akyu0002@algonquinlive.com>
2022-04-12 19:24:55 -0400

    Add logger from winston

--- startup/connectDatabase.js
+++ startup/connectDatabase.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import createDebug from "debug";
+import log from "./logger.js";
 
 export default function () {
   mongoose
     .connect("mongodb://localhost:27017/mad9124", { useNewUrlParser: true })
     .then(() => {
-      console.log("Successfully connected to MongoDB ...");
+      log.info("Successfully connected to MongoDB ...");
     })
     .catch((err) => {
-      console.log("Error connecting to MongoDB ... ", err.message);
+      log.info("Error connecting to MongoDB ... ", err.message);
       process.exit(1);
     });
 }
